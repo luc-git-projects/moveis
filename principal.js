039e627e5288dc235531f9e51980f30258872ec6
luc-git-projects <luc.git.projects@hotmail.com>
2024-02-16 13:26:42 -0300

    Inserindo arquivo principal.js

---
+++ principal.js
@@ -0,0 +1,7 @@
+var banners = ["O melhores do Brasil", "Qualidade e preço baixo"];
+var bannerAtual = 0;
+
+function trocarBanner(){
+    bannerAtual = (bannerAtual + 1 ) % 2;
+    document.querySelector('h2#mensagem').textContent = banners[bannerAtual]
+}
